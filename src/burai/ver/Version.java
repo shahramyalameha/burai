4f2cba8caa01deb4e2f09017d6cdb40644111f5b
nisihara1 <nisihara.burai@gmail.com>
2017-04-28 06:58:58 +0900

    change version 1.1 -> 1.2

--- src/burai/ver/Version.java
+++ src/burai/ver/Version.java
@@ -11,7 +11,7 @@ package burai.ver;
 
 public interface Version {
 
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 
     public static final double VERSION_NUMBER = Double.parseDouble(VERSION);
 
