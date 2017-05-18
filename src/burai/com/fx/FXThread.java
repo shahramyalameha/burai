70b4a435a78436b9f97cee10ef1aa3a693e4f2a8
nisihara1 <nisihara.burai@gmail.com>
2017-05-18 21:52:25 +0900

    add FXThread

---
+++ src/burai/com/fx/FXThread.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2017 Satomichi Nishihara
+ *
+ * This file is distributed under the terms of the
+ * GNU General Public License. See the file `LICENSE'
+ * in the root directory of the present distribution,
+ * or http://www.gnu.org/copyleft/gpl.txt .
+ */
+
+package burai.com.fx;
+
+import javafx.application.Platform;
+
+public class FXThread<R> {
+
+    private boolean inFX;
+
+    private boolean doneFX;
+
+    private R result;
+
+    private FXRunnable<? extends R> runnable;
+
+    public FXThread(FXRunnable<? extends R> runnable) {
+        if (runnable == null) {
+            throw new IllegalArgumentException("runnable is null.");
+        }
+
+        this.inFX = false;
+        this.doneFX = false;
+        this.result = null;
+        this.runnable = runnable;
+    }
+
+    public R getResult() {
+        this.start();
+        return this.result;
+    }
+
+    public void start() {
+        if (this.doneFX) {
+            return;
+        }
+
+        this.inFX = true;
+
+        Platform.runLater(() -> {
+            this.result = this.runnable.run();
+
+            synchronized (this) {
+                this.inFX = false;
+                this.notifyAll();
+            }
+        });
+
+        synchronized (this) {
+            while (this.inFX) {
+                try {
+                    this.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        this.doneFX = true;
+    }
+}
