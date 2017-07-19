b9a11b55422635c409fe278e8d98c7af1a923c98
nisihara1 <nisihara.burai@gmail.com>
2017-07-19 21:12:24 +0900

    update QEFXDesignerViewer

--- src/burai/app/project/viewer/designer/QEFXDesignerViewerController.java
+++ src/burai/app/project/viewer/designer/QEFXDesignerViewerController.java
@@ -12,6 +12,9 @@ package burai.app.project.viewer.designer;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import burai.app.QEFXAppController;
 import burai.app.project.QEFXProjectController;
 
@@ -19,15 +22,49 @@ public class QEFXDesignerViewerController extends QEFXAppController {
 
     private QEFXProjectController projectController;
 
+    @FXML
+    private Pane primPane;
+
+    @FXML
+    private Pane dualPane;
+
+    @FXML
+    private StackPane basePane;
+
     public QEFXDesignerViewerController(QEFXProjectController projectController) {
         super(projectController == null ? null : projectController.getMainController());
 
-        // TODO
+        this.projectController = projectController;
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        this.setupPrimPane();
+        this.setupDualPane();
+        this.setupBasePane();
+    }
+
+    private void setupPrimPane() {
+        if (this.primPane == null) {
+            return;
+        }
+
         // TODO
     }
 
+    private void setupDualPane() {
+        if (this.dualPane == null) {
+            return;
+        }
+
+        // TODO
+    }
+
+    private void setupBasePane() {
+        if (this.basePane == null) {
+            return;
+        }
+
+        // TODO
+    }
 }
